277480ba5e52bb1033e2a319a63d934336b7537f
ramya-sarikonda <ramyasarikonda2001@gmail.com>
2024-03-21 10:55:59 +0530

    new java file

---
+++ HotelAppRun.java
@@ -0,0 +1,13 @@
+package HotelAppRunner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features",glue="stepDefinitions")
+
+public class HotelAppRun {
+	
+
+}
